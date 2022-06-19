0b0f5de6bf97a58608950e7a2d2b044ef3b50d96
rishabh tiwari <postmailtorishabh@gmail.com>
2022-06-19 23:35:39 +0530

    first commit

---
+++ build/precache-manifest.6ae67bd00170d3b3f7292c1e8d4cb2f7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8283ede48d6b8ffb14a836920dc456f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaa1195ee5dceb2a2e56",
+    "url": "/static/js/2.6e44967c.chunk.js"
+  },
+  {
+    "revision": "db9c22bc02fdccdb664b",
+    "url": "/static/js/main.6de657fb.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
